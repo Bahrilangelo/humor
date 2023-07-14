0c8dcdd43948e5c252f37d1084ed1fe3f2fa7f73
bahrilangelo <bahri.official@protonmail.com>
2023-07-14 19:48:20 +0300

    new file

---
+++ The n word censor/index.py
@@ -0,0 +1,15 @@
+# H huh what is the n word..?
+# Dude if you say this word by the black people proably you will die :D
+# Man i'll write a code for a censor the n letter. 
+# Wow interesting solution but it seems a work :D
+
+def censor(sentence):
+    ans = ""
+    for i in sentence:
+        if i == "n":
+            ans += "*"
+        else:
+            ans += i
+    return ans 
+
+print(censor('Hello new world!'))
